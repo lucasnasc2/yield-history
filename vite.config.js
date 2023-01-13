63e81054da3162944b7796c895d09b33c7f51313
lucasnasc2 <lucas.nasc2.ln@gmail.com>
2023-01-13 01:32:18 +0100

    pwa config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,34 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.png"],
+      manifest: {
+        name: "Yeild History Logger",
+        short_name: "YHL",
+        description: "Keep track of the yeild on an investment.",
+        theme_color: "#ba4de3",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
